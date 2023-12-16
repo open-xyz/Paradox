1da096991af6118c08e24a17262d7d85ce43f660
Haider Patanwala <53524946+haider-patanwala@users.noreply.github.com>
2023-12-17 02:53:38 +0530

    fixed header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -21,27 +21,40 @@ const Header: React.FC<HeaderProps> = () => {
 	return (
 		<div className='fixed w-full top-[5%] flex justify-center'>
 			<div className='absolute aspect-square w-[80rem] bg-slate-600/20 blur-3xl rounded-full -z-10 -top-[30rem] animate-pulse  ease-in-out' />
-			<Menubar className='flex items-center justify-center gap-5 text-white border-none rounded-full w-fit px-9 bg-arcMenuBg'>
-				<MenubarMenu>
-					<MenubarTrigger asChild>
-						<Link href={'/'}>Home</Link>
-					</MenubarTrigger>
-				</MenubarMenu>
-				<MenubarMenu>
-					<MenubarTrigger>
-						<Link href={'/'}>Report Analyzer</Link>
-					</MenubarTrigger>
-				</MenubarMenu>
-				<MenubarMenu>
-					<MenubarTrigger>
-						<Link href={'/'}>Medicine Analyzer</Link>
-					</MenubarTrigger>
-				</MenubarMenu>
-				<MenubarMenu>
-					<MenubarTrigger>
-						<Link href={'/'}>Doctor AI</Link>
-					</MenubarTrigger>
-				</MenubarMenu>
+			<Menubar className='flex items-center justify-center gap-5 text-sm text-white border-none rounded-full w-fit px-9 bg-arcMenuBg'>
+				<Link
+					className='transition-all duration-300 ease-in-out hover:underline'
+					href={'/'}
+				>
+					Home
+				</Link>
+
+				<Link
+					className='transition-all duration-300 ease-in-out hover:underline'
+					href={'/medical-report'}
+				>
+					Report Analyzer
+				</Link>
+
+				<Link
+					className='transition-all duration-300 ease-in-out hover:underline'
+					href={'/medicine-analyzer'}
+				>
+					Medicine Analyzer
+				</Link>
+
+				<Link
+					className='transition-all duration-300 ease-in-out hover:underline'
+					href={'/physio'}
+				>
+					Physio
+				</Link>
+				<Link
+					className='transition-all duration-300 ease-in-out hover:underline'
+					href={'/chat'}
+				>
+					Doctor AI
+				</Link>
 			</Menubar>
 		</div>
 	);
